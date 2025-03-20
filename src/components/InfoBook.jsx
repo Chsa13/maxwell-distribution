b4460b0d1ebc0b98d0f58bf81c19cfadf8531d7e
Chsa13 <154126449+Chsa13@users.noreply.github.com>
2025-03-20 20:38:11 +0300

    1

--- src/components/InfoBook.jsx
+++ src/components/InfoBook.jsx
@@ -14,7 +14,7 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
-    width: '400px',
+    width: '370px',
     padding: '20px',
     borderRadius: '10px',
     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
